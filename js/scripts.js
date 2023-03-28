59c0973b227c401301503f9cf96d18602a5b3576
jessicahattig <jessicahattig@gmail.com>
2023-03-28 15:06:38 -0700

    Adding UI logic.

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,34 @@
 // Business Logic
 
 function wordCounter(text) {
-  if (text.length === 0) {
+  if (text.trim().length === 0) {
     return 0;
   }
   let wordCount = 0;
   const textArray = text.split(" ");
-  textArray.forEach(function(word) {
-    wordCount++;
+  textArray.forEach(function(element) {
+    if (!Number(element)) {
+      wordCount++;
+    }
   });
   return wordCount;
 }
 
-// Business Logic
-
-// wordCounter() function omitted for brevity.
-
 function numberOfOccurrencesInText(word, text) {
-  if (word === text) {
-    return 1;
-  }
-  return 0;
+  const textArray = text.split(" ");
+  let wordCount = 0;
+  textArray.forEach(function(element) {
+    if (element.toLowerCase().includes(word.toLowerCase())) {
+      wordCount++;
+    }
+  });
+  return wordCount;
 }
+
+// UI Logic
+
+
+
+window.addEventListener("load", function() {
+  document.querySelector("form#word-counter").addEventListener("submit", handleFormSubmission);
+});
